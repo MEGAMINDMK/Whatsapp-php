257b6bcfd39a205d6eb74809c10732f0c20e5e9b
webnet <muhommadraahim1@gmail.com>
2019-07-29 14:57:24 +0500

    Create send.php

---
+++ send.php
@@ -0,0 +1,9 @@
+$num = $_GET['tonum'];
+<a href="https://api.whatsapp.com/send?phone=<?php echo $num; ?>&text=I am <?php echo $_GET['name']; ?>, 
+My number <?php echo $_GET['number']; ?>, 
+I want to book my tour at date: <?php echo $_GET['dot']; ?>, 
+Time: <?php echo $_GET['ptime']; ?>, 
+Persons: <?php echo $_GET['nop']; ?>, 
+Want to visit <?php echo $_GET['destination']; ?>, 
+If you could please pickup us Thanks" class="btn btn-warning btn-lg">Book my Tour</a>
+  
